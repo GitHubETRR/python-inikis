103083e47d0e4a7986c23db67dffcd0da34ef81c
inikis <dinovelley@gmail.com>
2024-11-21 16:52:22 -0300

    Cine grid v0.0.2
    cambio nombre archivo main

--- cine-grid/cine_grid.py
+++ cine-grid/cine_grid.py
@@ -18,7 +18,7 @@ for item1 in TEMAS[0]:
             #print(f"{nuevaCombinacion}, {combinaciones[nuevaCombinacion]}")
             cant_combinaciones += 1
 
-#print(cant_combinaciones)
+print(cant_combinaciones)
 
 opcion_x, opcion_y, opcion_z = random.sample(range(0, len(TEMAS)), 3)
 tema_x, tema_y, tema_z = TEMAS[opcion_x], TEMAS[opcion_y], TEMAS[opcion_z]
@@ -39,10 +39,18 @@ coord_usuario = (int(coord_x_usuario)-1,int(coord_y_usuario)-1)
 
 grilla[coord_usuario[1]+1][coord_usuario[0]+1] = tema_x[coord_usuario[0]] + tema_y[coord_usuario[1]]
 """
-input_usuario = str(input(f"Elija uno de los siguientes valores: {tema_z[0],tema_z[1],tema_z[2]}"))
+input_usuario = input(f"Elija uno de los siguientes valores {tema_z[0],tema_z[1],tema_z[2]}: ")
+#TODO: las combinaciones usan el orden de los TEMAS (may, min, num), en cambio los elegidos estan en lo que quedo para X, Y, Z
 
-for combinacion in combinaciones:
-    print(combinacion)
+for item1 in tema_x:
+    for item2 in tema_y:
+        evaluacion=item1+item2+input_usuario
+        print(evaluacion)
+        try:
+            if combinaciones[evaluacion] == True:
+                print(f"Acertaste con: {item1,item2,input_usuario}")
+        except KeyError:
+            pass
 
 for fila in grilla:
     print(fila)
